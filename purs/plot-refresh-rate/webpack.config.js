a75143ab139829b4fa169dacf38ef2c08e06f0b2
nathan <ntwilson@gmail.com>
2019-07-27 02:07:16 -0600

    setup electron

--- purs/plot-refresh-rate/webpack.config.js
+++ purs/plot-refresh-rate/webpack.config.js
@@ -4,12 +4,12 @@ module.exports = {
   entry: './src/app.js',
   mode: 'development',
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'public'),
     compress: true,
     port: 9000
   },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'public')
   }
 };
