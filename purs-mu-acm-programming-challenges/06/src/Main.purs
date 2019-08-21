95c32a4a1a5450285a7778d6b6095f3d5a8cb6ea
nathan <ntwilson@gmail.com>
2019-08-21 00:00:26 -0400

    better error handling

--- purs-mu-acm-programming-challenges/06/src/Main.purs
+++ purs-mu-acm-programming-challenges/06/src/Main.purs
@@ -1,7 +1,13 @@
-module Main where
+module Main 
+  ( Base
+  , baseFromInt
+  , Digit
+  , digitFromInt
+  , baseConverter
+  , multBaseConverter
+  , main) where
 
 import Prelude
-
 
 import Data.Array ((..))
 import Data.Char (fromCharCode, toCharCode)
@@ -12,77 +18,133 @@ import Data.Maybe (Maybe(..))
 import Data.Maybe.FromJustUnless (fromJustUnless, unsafeFromJustUnless)
 import Data.String.CodeUnits (singleton)
 import Effect (Effect)
-
-import Effect.Console (logShow)
+import Effect.Console (log, logShow)
 import Effect.Promise (class Deferred, Promise, promise, runPromise)
 import Node.ReadLine (Interface, close, createConsoleInterface, noCompletion, question, setPrompt)
 import Partial.Unsafe (unsafePartial)
 
-baseConverter :: Int -> Int -> String 
-baseConverter testNum base = 
+newtype Base = Base Int
+baseFromInt :: Int -> Maybe Base
+baseFromInt i
+  | 2 <= i && i <= 62 = Just $ Base i
+  | otherwise = Nothing
+
+range :: Base -> Base -> Array Base
+range (Base lower) (Base upper) = do
+  ibase <- lower .. upper 
+  pure $ Base ibase
+
+-- an Int 0 - maxDigit. 10 numeric digits + 26 upper case letter digits + 26 lower case letter digits
+newtype Digit = Digit Int
+newtype LetterIndex = LetterIndex Int -- 0 - 25 ('a' - 'z')
+
+data DigitRenderer 
+  = Direct Int -- 0 - 9
+  | Upper LetterIndex  -- 0 - 25 ('A' - 'Z')
+  | Lower LetterIndex  -- 0 - 25 ('a' - 'z')
+
+maxDigit :: Int 
+maxDigit = 
+  let 
+    numDirectDigits = 10
+    numUpperDigits = 26
+    numLowerDigits = 26
+  in
+    numDirectDigits + numUpperDigits + numLowerDigits - 1
+
+digitFromInt :: Int -> Maybe Digit
+digitFromInt i 
+  | 0 <= i && i <= maxDigit = Just $ Digit i
+  | otherwise = Nothing
+
+renderer :: Digit -> DigitRenderer
+renderer (Digit i)
+  | 0 <= i && i < 10 = Direct i
+  | (let x = i - 10 in 0 <= x && x < 26) = Upper $ LetterIndex $ i - 10
+  | otherwise = Lower $ LetterIndex $ i - 10 - 26
+
+renderDigit :: Digit -> String
+renderDigit d =
+  case renderer d of
+    Direct i -> show i
+    Upper (LetterIndex i) -> 
+      let 
+        valOfA = toCharCode 'A'
+        charVal = valOfA + i
+        char = fromCharCode charVal # unsafeFromJustUnless ("can't add letter index " <> show i <> " to character 'A'")
+      in singleton char
+
+    Lower (LetterIndex i) ->
+      let 
+        valOfA = toCharCode 'a'
+        charVal = valOfA + i
+        char = fromCharCode charVal # unsafeFromJustUnless ("can't add letter index " <> show i <> " to character 'a'") 
+      in singleton char
+
+baseConverter :: Int -> Base -> String 
+baseConverter testNum (Base base) = 
   let
     baseDigits = List.iterate (_ * base) base
     relevantDigits = 1 : (baseDigits # List.takeWhile (_ <= testNum)) # List.reverse
-    render i
-      | 0 <= i && i < 10 = show i
-      | 10 <= i && i <= 35 = 
-        let 
-          valOfA = toCharCode 'A'
-          charVal = valOfA + (i - 10)
-          char = fromCharCode charVal # unsafeFromJustUnless ("unable to add " <> show (i - 10) <> " to character 'A'")
-        in singleton char
-
-      | otherwise =
-        let 
-          valOfA = toCharCode 'a'
-          charVal = valOfA + (i - 36)
-          char = fromCharCode charVal # unsafeFromJustUnless ("Given too large of a digit: " <> show i <> ". Must be able to add " <> show (i - 37) <> " to character 'a'")
-        in singleton char
 
     iterate digits (remainder :: Int) =
       case List.uncons digits of
-        Just { head: nextDigit, tail: remainingDigits } ->
+        Just { head: thisDigitRoot, tail: remainingDigits } ->
           let
-            thisDigitValue = remainder / nextDigit
-            newRemainder = remainder - (thisDigitValue * nextDigit)
-            thisDigit = render thisDigitValue
+            thisDigitValue = remainder / thisDigitRoot
+            newRemainder = remainder - (thisDigitValue * thisDigitRoot)
+            thisDigit = 
+              digitFromInt thisDigitValue 
+              # unsafeFromJustUnless ("Bad logic computing base convertion for base " <> show base 
+                <> ". Remainder " <> show remainder <> " while considering digit " <> show thisDigitRoot 
+                <> " resulted in a value that cannot be rendered as a digit: " <> show thisDigitValue)
           in 
-            thisDigit <> iterate remainingDigits newRemainder
+            renderDigit thisDigit <> iterate remainingDigits newRemainder
         Nothing -> ""
 
   in iterate relevantDigits testNum
 
-multBaseConverter :: Int -> Int -> Int -> Array String 
+multBaseConverter :: Int -> Base -> Base -> Array String 
 multBaseConverter testNum lBound uBound = do
-  base <- lBound .. uBound
+  base <- range lBound uBound
   pure $ baseConverter testNum base
 
-ask :: Deferred => String -> Interface -> Promise String
-ask prompt iface = 
-  promise \resolve err -> question prompt resolve iface
+ask :: Deferred => String -> Promise String
+ask prompt = 
+  promise \resolve err -> do
+    iface <- createConsoleInterface noCompletion
+    let 
+      resolveAndClose x = do
+        resolve x
+        close iface
 
-fromJust :: forall a. Partial => Maybe a -> a
-fromJust = fromJustUnless "Unrecognized input.  Expecting a number."
+    question prompt resolveAndClose iface
 
-getInput :: Partial => Deferred => Interface -> Promise { lBound :: Int, testNum :: Int, uBound :: Int }
-getInput iface = do
-  testNumStr <- ask "please enter a number to convert: " iface
-  let testNum = fromJust $ Int.fromString testNumStr
-  lBoundStr <- ask "please enter a lower-bound base:  " iface
-  let lBound = fromJust $ Int.fromString lBoundStr
-  uBoundStr <- ask "please enter an upper-bound base: " iface
-  let uBound = fromJust $ Int.fromString uBoundStr
+
+getInput :: Partial => Deferred => Promise { testNum :: Int, lBound :: Base, uBound :: Base }
+getInput = do
+  testNumStr <- ask "please enter a number to convert: "
+  let testNum = fromJustUnless "Unrecognized input.  Expecting a number." $ Int.fromString testNumStr
+
+  lBoundStr <- ask "please enter a lower-bound base:  " 
+  let 
+    lBound = 
+      fromJustUnless "Unrecognized input. Expecting a number between 2 and 62" $ 
+        Int.fromString lBoundStr >>= baseFromInt
+
+  uBoundStr <- ask "please enter an upper-bound base: "
+  let 
+    uBound = 
+      fromJustUnless "Unrecognized input. Expecting a number between 2 and 62" $ 
+        Int.fromString uBoundStr >>= baseFromInt
+
   pure { testNum, lBound, uBound }
 
 main :: Effect Unit
-main = unsafePartial do
-  iface <- createConsoleInterface noCompletion
-  setPrompt "> " 2 iface
-  
+main = unsafePartial $
   runPromise 
     (\{ testNum, lBound, uBound } -> do
-      logShow $ multBaseConverter testNum lBound uBound
-      close iface)
-    (\_ -> close iface)
-    (getInput iface)
+      logShow $ multBaseConverter testNum lBound uBound)
+    (\err -> logShow err)
+    getInput
 
--- purs-mu-acm-programming-challenges/06/test/Main.purs
+++ purs-mu-acm-programming-challenges/06/test/Main.purs
@@ -3,46 +3,52 @@ module Test.Main where
 import Prelude
 
 import Data.Identity (Identity)
+import Data.Maybe.FromJustUnless (fromJustUnless)
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
-import Main (baseConverter, multBaseConverter)
+import Main (Base, baseConverter, baseFromInt, multBaseConverter)
+import Partial.Unsafe (unsafePartial)
 import Test.Spec (SpecT, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
 
-refreshingPlotSpecs :: SpecT Aff Unit Identity Unit
-refreshingPlotSpecs = do
+mkBase :: Partial => Int -> Base
+mkBase i = baseFromInt i # fromJustUnless ("bad test. Trying to create a Base of " <> show i)
+
+baseConversionSpecs :: SpecT Aff Unit Identity Unit
+baseConversionSpecs = unsafePartial do
   describe "number base conversions" do
     describe "with a single target base" do
       it "converts a decimal number to the supplied base" do
-        baseConverter 5 2 `shouldEqual` "101"
-        baseConverter 11 2 `shouldEqual` "1011"
-        baseConverter 5 3 `shouldEqual` "12"
-        baseConverter 7 3 `shouldEqual` "21"
+        baseConverter 5 (mkBase 2) `shouldEqual` "101"
+        baseConverter 11 (mkBase 2) `shouldEqual` "1011"
+        baseConverter 5 (mkBase 3) `shouldEqual` "12"
+        baseConverter 7 (mkBase 3) `shouldEqual` "21"
 
       it "handles converting 0" do
-        baseConverter 0 2 `shouldEqual` "0"
-        baseConverter 0 5 `shouldEqual` "0"
+        baseConverter 0 (mkBase 2) `shouldEqual` "0"
+        baseConverter 0 (mkBase 5) `shouldEqual` "0"
     
     describe "with multiple target bases" do
       it "converts to all bases between an upper and lower bound" do
-        multBaseConverter 7 2 8 `shouldEqual` ["111","21","13","12","11","10","7"]
-        multBaseConverter 0 2 8 `shouldEqual` [  "0", "0", "0", "0", "0", "0","0"]
+        multBaseConverter 7 (mkBase 2) (mkBase 8) `shouldEqual` ["111","21","13","12","11","10","7"]
+        multBaseConverter 0 (mkBase 2) (mkBase 8) `shouldEqual` [  "0", "0", "0", "0", "0", "0","0"]
 
     describe "with bases that use all available character sets" do
       it "gets the boundaries correct" do
-        baseConverter 9 40 `shouldEqual` "9"
-        baseConverter 10 40 `shouldEqual` "A"
-        baseConverter 35 40 `shouldEqual` "Z"
-        baseConverter 36 40 `shouldEqual` "a"
+        baseConverter 9 (mkBase 40) `shouldEqual` "9"
+        baseConverter 10 (mkBase 40) `shouldEqual` "A"
+        baseConverter 35 (mkBase 40) `shouldEqual` "Z"
+        baseConverter 36 (mkBase 40) `shouldEqual` "a"
+        baseConverter 61 (mkBase 62) `shouldEqual` "z"
 
   describe "acceptance criteria" do
     it "gives the correct answer for the problems provided in the writeup" do
-      multBaseConverter 10 2 4 `shouldEqual` ["1010", "101", "22"]
-      multBaseConverter 100 15 20 `shouldEqual` ["6A", "64", "5F", "5A", "55", "50"]
+      multBaseConverter 10 (mkBase 2) (mkBase 4) `shouldEqual` ["1010", "101", "22"]
+      multBaseConverter 100 (mkBase 15) (mkBase 20) `shouldEqual` ["6A", "64", "5F", "5A", "55", "50"]
 
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] do
-  refreshingPlotSpecs
+  baseConversionSpecs
