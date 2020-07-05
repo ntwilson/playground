16cca63b8e5a08fb3d7beafb47908af663c57c05
nathan <nathan.wilson@marquetteenergyanalytics.com>
2020-07-05 14:40:33 -0500

    more idiomatic error handling

--- purs/purescript-mssql/src/Main.purs
+++ purs/purescript-mssql/src/Main.purs
@@ -2,62 +2,63 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Except (Except, ExceptT(..), except, runExcept, runExceptT, withExcept, withExceptT)
+import Control.Monad.Error.Class (class MonadThrow, throwError)
+import Control.Monad.Except (runExceptT)
 import Data.Either (Either(..))
+import Data.List.NonEmpty (NonEmptyList)
 import Data.Newtype (unwrap)
 import Data.Traversable (traverse)
 import Database (Connection, close, connect, query)
 import Database.ORM (KeyedValues, KeyedValuesDecoder, WeatherValues, WeatherValuesDecoder)
 import Effect (Effect)
-import Effect.Aff (Aff, launchAff_, message)
+import Effect.Aff (Aff, attempt, launchAff_)
 import Effect.Class (class MonadEffect)
 import Effect.Class.Console (log)
+import Effect.Exception (Error, error, message)
+import Foreign (ForeignError)
 import Foreign.Generic (class Decode, Foreign, decode)
 
-decode' :: forall m a. Monad m => Decode a => Foreign -> ExceptT String m a
-decode' = decode >>> withExcept show >>> switchMonad
+decode' :: forall a. Decode a => Foreign -> Either (NonEmptyList ForeignError) a
+decode' = decode >>> runExceptT >>> unwrap
 
-switchMonad :: forall e m a. Monad m => Except e a -> ExceptT e m a 
-switchMonad = runExcept >>> except
+eitherToM :: forall m e a. MonadEffect m => MonadThrow Error m => Show e => Either e a -> m a
+eitherToM (Left e) = throwError (error (show e)) 
+eitherToM (Right a) = pure a
 
 
-getKeyedValues :: Connection -> Aff (Either String (Array KeyedValues))
-getKeyedValues conn = runExceptT $ do
-  result <- query conn "SELECT * FROM KeyedValues" # ExceptT # withExceptT message
-  (keyedValues :: Array KeyedValuesDecoder) <- traverse decode' result 
+getKeyedValues :: Connection -> Aff (Array KeyedValues)
+getKeyedValues conn = do
+  result <- query conn "SELECT * FROM KeyedValues" 
+  (keyedValues :: Array KeyedValuesDecoder) <- traverse (decode' >>> eitherToM) result 
   pure (unwrap <$> keyedValues)
 
 
-getAFewWeatherValues :: Connection -> Aff (Either String (Array WeatherValues)) 
-getAFewWeatherValues conn = runExceptT $ do
-  results <- query conn "SELECT TOP 10 * FROM weather_values" # ExceptT # withExceptT message
-  (keyedValues :: Array WeatherValuesDecoder) <- traverse decode' results
+getAFewWeatherValues :: Connection -> Aff (Array WeatherValues)
+getAFewWeatherValues conn = do
+  results <- query conn "SELECT TOP 10 * FROM weather_values" 
+  (keyedValues :: Array WeatherValuesDecoder) <- traverse (decode' >>> eitherToM) results
   pure (unwrap <$> keyedValues)
 
 
 main :: Effect Unit
 main = launchAff_ do
   
-  tryConn <- connect "mssql03.ad.mea.energy" "SystemTestSource" 
-  case tryConn of 
-    Right conn -> do
-      keyedValues <- getKeyedValues conn
-      showResults "KeyedValues" keyedValues
+  conn <- connect "mssql03.ad.mea.energy" "SystemTestSource" 
+  keyedValues <- attempt (getKeyedValues conn)
+  showResults "KeyedValues" keyedValues
 
-      wxValues <- getAFewWeatherValues conn
-      showResults "WeatherValues" wxValues
+  wxValues <- attempt (getAFewWeatherValues conn)
+  showResults "WeatherValues" wxValues
 
-      _ <- close conn
-      pure unit
+  _ <- close conn
+  pure unit
 
-    Left error -> 
-      log ("Could not even connect to the database. " <> message error)
 
   where
-    showResults :: forall r m. Show r => MonadEffect m => String -> Either String (Array r) -> m Unit
+    showResults :: forall r m. Show r => MonadEffect m => String -> Either Error (Array r) -> m Unit
     showResults name results = 
       case results of
         Right values -> log ("Got " <> name <> "! " <> show values) 
-        Left error -> log ("uh oh. " <> error)
+        Left error -> log ("uh oh. " <> message error)
 
 
